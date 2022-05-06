4cb851f743b66de5407e7e37efa2144c61270c93
k <56704743+karruc@users.noreply.github.com>
2022-05-06 14:39:18 +0200

    Add files via upload

---
+++ labb5/labb5/programmet/inputHandler.c
@@ -0,0 +1,24 @@
+/*
+ * inputHandler.c
+ *
+ * Created: 2022-05-04 11:06:34
+ *  Author: 
+ */ 
+
+#include "inputHandler.h"
+
+void input(IH *self) {
+	// ?????
+	while (!(UCSR0A & (1 << RXC0)));
+	volatile uint8_t indata = UDR0; // does it have to be volatile?
+		
+	if (indata & nCar) {
+		ASYNC(self->controller, newQueue, 1); // Adding to the northbound queue
+	} else if (indata & nBridge) {
+		ASYNC(self->controller, newBridge, 1); // Adding a car on the bridge towards north
+	} else if (indata & sCar) {
+		ASYNC(self->controller, newQueue, 2); // Adding to the southbound queue
+	} else if (indata & sBridge) {
+		ASYNC(self->controller, newBridge, 2); // Adding a car on the bridge towards south
+	}
+}
